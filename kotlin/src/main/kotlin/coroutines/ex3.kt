1d3d41cc3652e21e7b6749678e5293cd03062b41
zhouweixian <laxian2010@qq.com>
2019-10-26 17:01:31 +0800

    kotlin coroutines exercise

---
+++ kotlin/src/main/kotlin/coroutines/ex3.kt
@@ -0,0 +1,12 @@
+package coroutines
+
+import kotlinx.coroutines.*
+
+fun main() = runBlocking<Unit> { // 开始执行主协程
+    GlobalScope.launch { // 在后台启动一个新的协程并继续
+        delay(1000L)
+        println("World!")
+    }
+    println("Hello,") // 主协程在这里会立即执行
+    delay(2000L)      // 延迟 2 秒来保证 JVM 存活
+}
